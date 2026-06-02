99533bbbc3ceabd1681a46903e02a67b9c1186dd
Krish-Jaiswal27 <krishjaiswal1027@gmail.com>
2026-06-02 08:35:44 +0530

    backend deploy url fetch change

--- frontend/script.js
+++ frontend/script.js
@@ -21,7 +21,7 @@ async function predictIrrigation() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/predict",
+            "https://the-irrigator.onrender.com/predict",
             {
                 method: "POST",
                 headers: {
